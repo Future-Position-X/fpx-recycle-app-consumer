37645e3a43da923229d55d2db8300c05a4d1e0ae
Andreas Franzén <andreas@devil.se>
2020-10-02 15:59:03 +0200

    Style App

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,11 +1,52 @@
 <template>
-    <Page @loaded="onPageLoaded" actionBarHidden="true" style="margin: 5px;">
+  <Page actionBarHidden="true" background="#f2f2f2">
+    <GridLayout>
+      <ScrollView>
+        <GridLayout rows="auto, *" padding="0 0">
+          <Image row="0" src="~/assets/images/dots.png" stretch="fill" horizontalAlignment="left" verticalAlignment="top" marginTop="30" marginLeft="25" width="25" height="25"/>
+          <Image row="0" src="~/assets/images/Pantr_logo@3x.png" stretch="fill" horizontalAlignment="center" verticalAlignment="top" width="150" height="35" marginTop="25"/>
+          <Image row="0" src="~/assets/images/icon_help@3x.png" stretch="fill" horizontalAlignment="right" verticalAlignment="top" marginTop="30" marginRight="25" width="25" height="25"/>
+          <StackLayout row="1" marginTop="18">
+            <StackLayout marginTop="3" padding="0" margin="0 10">
+              <Label text="Hämtningar i ditt område" fontWeight="bold" fontSize="21" class="titleColor" />
+              <GridLayout v-for="(item, index) in pantRuns" :key="item.id" columns="auto,*" borderBottomColor="#ddd" :borderBottomWidth="index == pantRuns.length - 1 ? 0 : 1" padding="10 0" margin="0 10">
+                <Image col="0" src="~/assets/images/icon_clock@3x.png" stretch="fill" verticalAlignment="center" marginRight="15" width="30" height="30"/>
+                <StackLayout col=1>
+                  <Label margin="2 0" textWrap="true" fontSize="18" class="bodyTextColor">{{item.time}}</Label>
+                  <Label class="bodyTextColor" textWrap="true" margin="2 0" fontSize="18">
+                    <FormattedString>
+                      <Span text="Hämtare: "/>
+                      <Span fontWeight="bold">{{item.collector}}</Span>
+                    </FormattedString>
+                  </Label>
+                </StackLayout>
+              </GridLayout>
+              <Label text="Dina bookade hämtningar" fontWeight="bold" fontSize="21" class="titleColor" marginTop="20" />
+              <GridLayout v-for="(item, index) in bookedRuns" :key="item.id" columns="*, auto" padding="10 15" margin="5 10" borderRadius="20" backgroundColor="white" androidElevation="12">
+                <StackLayout col=0>
+                  <Label margin="2 0" textWrap="true" fontSize="18" class="bodyTextColor" fontWeight="bold">{{item.title}}</Label>
+                  <Label class="bodyTextColor" textWrap="true" margin="2 0" fontSize="18" fontStyle="italic">{{item.text}}</Label>
+                </StackLayout>
+                <Image col="1" :src="statusImages[item.status]" stretch="fill" verticalAlignment="center" marginRight="15" width="30" height="30"/>
+              </GridLayout>
+            </StackLayout>
+            </StackLayout>
+        </GridLayout>
+      </ScrollView>
+      <GridLayout v-if="showThankYou" borderRadius="20" verticalAlignment="center" horizontalAlignment="center" background="white" androidElevation="12" margin="30" padding="30">
+        <StackLayout>
+          <Image src="~/assets/images/icon_heart@3x.png" stretch="none" horizontalAlignment="center" />
+          <Label text="Tack!" fontWeight="bold" fontSize="37" class="titleColor" horizontalAlignment="center" marginTop="10" />
+          <Label text="Genom att skänka pant så hjälper du skolor och föreningsliv i ditt närområde att skapa bra förutsättningar för ett välmående samhälle" marginTop="10" textWrap="true" lineHeight="3" fontSize="18" class="bodyTextColor" />
+          <Button @onTap="onContinue" text="Fortsätt" verticalAlignment="bottom" marginTop="39" textTransform="none" background="#1f2d40" color="white" borderRadius="40" width="60%" height="57" fontSize="21" class="bodyTextColor"/>
+        </StackLayout>
+      </GridLayout>
+    </GridLayout>
+  </Page>
+
+    <!--
+    <Page @loaded="onPageLoaded" actionBarHidden="true">
         <GridLayout rows="auto, *, auto, *, auto, *, auto, *, auto, *, auto">
-          <StackLayout row="0" style="border-width: 2px; border-radius: 5px 5px 5px 5px; border-color: #c4c1c0;">
-            <Label text="Håll dig uppdaterad genom pushnotifikationer" fontWeight="bold" horizontalAlignment="center"/>
-            <Label textWrap="true" horizontalAlignment="center" text="Pushar är det snabbaste och smidigaste sättet att få information om när din pant hämtas och/eller när det finns annan panthämtning inbokad i ditt område."/>
-            <Button @onTap="onAllowNotificationsTap" text="Tillåt pushnotifikationer"/>
-          </StackLayout>
           <StackLayout row="2">
             <Label text="Hämtningar i ditt område" horizontalAlignment="center" fontWeight="bold"/>
             <Label text="Onsdag 21/11 18:00-20:00 Gävle fotboll"/>
@@ -22,9 +63,15 @@
             <Label text="Kontakta oss:" horizontalAlignment="center"/>
             <Label text="x@y.se" horizontalAlignment="center"/>
           </StackLayout>
+          <StackLayout row="9" style="border-width: 2px; border-radius: 5px 5px 5px 5px; border-color: #c4c1c0;">
+            <Label text="Håll dig uppdaterad genom pushnotifikationer" fontWeight="bold" horizontalAlignment="center"/>
+            <Label textWrap="true" horizontalAlignment="center" text="Pushar är det snabbaste och smidigaste sättet att få information om när din pant hämtas och/eller när det finns annan panthämtning inbokad i ditt område."/>
+            <Button @onTap="onAllowNotificationsTap" text="Tillåt pushnotifikationer"/>
+          </StackLayout>
           <Button @onTap="onBookNewTap" row="10" text="Boka ny hämtning"/>
         </GridLayout>
     </Page>
+    -->
 </template>
 
 <script>
@@ -35,7 +82,49 @@
   export default {
     data() {
       return {
-        recycleCollectionItems: null
+        recycleCollectionItems: null,
+        statusImages: {
+          "waiting": "~/assets/images/icon_delete@3x.png",
+          "confirmed": "~/assets/images/icon_pantr_on_way@3x.png",
+          "done": "~/assets/images/icon_done@3x.png",
+        },
+        bookedRuns: [
+          {
+            id:1,
+            title: "2020-09-28 - Bokad hämtning",
+            text: "Gävle fotboll",
+            status: "waiting",
+          },
+          {
+            id:2,
+            title: "2020-09-28 - Bokad hämtning",
+            text: "Gävle ridklubb",
+            status: "confirmed",
+          },
+          {
+            id:3,
+            title: "2020-08-14 - Bokad hämtning",
+            text: "Gävle hockey",
+            status: "done",
+          },
+        ],
+        pantRuns: [
+          {
+            id:1,
+            time: "Onsdag 21/11 18:00-20:00",
+            collector: "Gävle fotboll",
+          },
+          {
+            id:2,
+            time: "Onsdag 21/11 18:00-20:00",
+            collector: "Gävle ridklubb",
+          },
+          {
+            id:3,
+            time: "Onsdag 21/11 18:00-20:00",
+            collector: "Gävle hockey",
+          },
+        ]
       }
     },
     methods: {
---
+++ reload.sh
@@ -0,0 +1,1 @@
+echo "\n//" >> app/main.js
