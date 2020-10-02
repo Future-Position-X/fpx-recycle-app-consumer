af205ed2bd01de316749747a999df044e1d7af62
warbler <33575360+warbler@users.noreply.github.com>
2020-10-02 11:44:04 +0200

    update info view

--- app/components/Info.vue
+++ app/components/Info.vue
@@ -1,71 +1,47 @@
 <template>
-    <Page actionBarHidden="true" style="margin: 5px;" background="#f2f2f2">
-        <GridLayout rows="auto, *, auto" marginLeft="15">
+    <Page actionBarHidden="true" background="#f2f2f2">
+        <ScrollView>
+            <GridLayout rows="auto, *, auto" padding="0 20">
             <Image row="0" src="~/assets/images/dots.png" stretch="fill" horizontalAlignment="left" verticalAlignment="top" marginTop="30" marginLeft="25" width="25" height="25"/>
             <Image row="0" src="~/assets/images/Pantr_logo@3x.png" stretch="fill" horizontalAlignment="center" verticalAlignment="top" width="150" height="35" marginTop="25"/>
             <Image row="0" src="~/assets/images/icon_help@3x.png" stretch="fill" horizontalAlignment="right" verticalAlignment="top" marginTop="30" marginRight="25" width="25" height="25"/>
             <StackLayout row="1" marginTop="18">
-                <Label text="Välkommen!" fontWeight="bold" fontSize="23" class="primaryTextColor" />
-                <Label text="Hur bor du?" fontWeight="bold" fontSize="18" class="primaryTextColor" marginTop="16" />
+                <Label text="Inställningar" fontWeight="bold" fontSize="23" class="primaryTextColor" />
+                <Label text="Hur bor du?" fontWeight="bold" fontSize="18" class="primaryTextColor" marginTop="22" />
                 <SegmentedBar selectedIndex="0" v-model="selectedPropertyType" background="#e3e3e4" selectedBackgroundColor="white" borderRadius="10" height="35" marginTop="10">
                     <SegmentedBarItem title="Villa" />
                     <SegmentedBarItem title="Lägenhet" />
                 </SegmentedBar>
-                <Label text="Inom vilken tidsram kan vi hämta panten?" fontWeight="bold" fontSize="18" class="primaryTextColor" marginTop="16" />
-                <SegmentedBar selectedIndex="0" v-model="selectedTimeFrame" background="#e3e3e4" selectedBackgroundColor="white" borderRadius="10" height="35" marginTop="10">
+                <Label text="Inom vilken tidsram kan vi hämta panten?" fontWeight="bold" fontSize="18" class="primaryTextColor" marginTop="22" />
+                <SegmentedBar selectedIndex="3" background="#e3e3e4" selectedBackgroundColor="white" borderRadius="10" height="35" marginTop="10">
                     <SegmentedBarItem title="12h" />
                     <SegmentedBarItem title="24h" />
                     <SegmentedBarItem title="48h" />
                     <SegmentedBarItem title="72h" />
                 </SegmentedBar>
-                <Label text="Panthämtare" fontWeight="bold" fontSize="18" class="primaryTextColor" marginTop="16" />
-                <GridLayout marginTop="10" background="white" height="50" borderRadius="30" borderWidth="1" borderColor="#e7e7e8">
-                    <DropDown ref="pantRetrieversDropDown" :items="pantRetrievers" selectedIndex="selectedPantRetriever" />
+                <Label row=0 text="Storlek på panten" fontWeight="bold" fontSize="18" class="primaryTextColor" marginTop="22" />
+                <GridLayout rows="auto, auto" columns="*, *, *" marginTop="10">
+                    <Image stretch="none" horizontalAlignment="center" verticalAlignment="bottom" row="0" column="0" src="~/assets/images/icon_small_load@3x.png"/>
+                    <Image stretch="none" horizontalAlignment="center" verticalAlignment="bottom" row="0" column="1" marginRight="25" src="~/assets/images/icon_small_load@3x.png"/>
+                    <Image stretch="none" horizontalAlignment="center" verticalAlignment="bottom" row="0" column="1" marginLeft="25" src="~/assets/images/icon_small_load@3x.png"/>
+                    <Image stretch="none" horizontalAlignment="center" row="0" column="2" src="~/assets/images/icon_big_load@3x.png"/>
+                    <SegmentedBar row="1" column="0" columnSpan="7" selectedIndex="0" v-model="selectedTimeFrame" background="#e3e3e4" selectedBackgroundColor="white" borderRadius="10" height="35" marginTop="10">
+                        <SegmentedBarItem title="Liten" />
+                        <SegmentedBarItem title="Mellan" />
+                        <SegmentedBarItem title="Stor" />
+                    </SegmentedBar>
                 </GridLayout>
-                <Label text="Övrig information" fontWeight="bold" fontSize="18" class="primaryTextColor" marginTop="16" />
+                <Label text="Panthämtare" fontWeight="bold" fontSize="18" class="primaryTextColor" marginTop="22" />
+                <GridLayout paddingLeft="15" marginTop="10" background="white" height="50" borderRadius="30" borderWidth="1" borderColor="#e7e7e8">
+                    <DropDown style="font-size: 18px;" ref="pantRetrieversDropDown" :items="pantRetrievers" selectedIndex="selectedPantRetriever" itemsPadding="20" itemsTextAlignment="center" verticalAlignment="middle"/>
+                </GridLayout>
+                <Label text="Övrig information" fontWeight="bold" fontSize="18" class="primaryTextColor" marginTop="22" />
                 <TextView marginTop="10" background="white" height="55" borderRadius="30" borderWidth="1" borderColor="#e7e7e8"/>
+                <Button row="2" @onTap="onBookTap" text="Boka hämtning" verticalAlignment="bottom" textTransform="none" background="#0aa67a" color="white" borderRadius="40" width="60%" height="57" fontSize="21" marginTop="40"/>
+            <Button row="2" text="Byt plats" verticalAlignment="bottom" textTransform="none" background="#1f2d40" color="white" borderRadius="40" width="60%" height="57" fontSize="21" marginTop="30" marginBottom="50"/>
             </StackLayout>
-            <Button row="2" @onTap="onBookTap" text="Boka hämtning" verticalAlignment="bottom" textTransform="none" background="#0aa67a" color="white" borderRadius="40" width="60%" height="57" fontSize="21" marginBottom="92"/>
-            <Button row="2" text="Byt plats" verticalAlignment="bottom" textTransform="none" background="#1f2d40" color="white" borderRadius="40" width="60%" height="57" fontSize="21" marginBottom="30"/>
         </GridLayout>
-        <!--<ScrollView>
-            <GridLayout rows="auto, *, auto, *, auto, *, auto, *, auto, *, auto, *, auto">
-                <Label row="0" text="Information" fontWeight="bold" horizontalAlignment="center"/>
-                <StackLayout row="2">
-                    <Label text="Bor du i villa eller lägenhet?" fontWeight="bold" horizontalAlignment="center"/>
-                    <SegmentedBar selectedIndex="0" v-model="selectedPropertyType">
-                        <SegmentedBarItem title="Villa" />
-                        <SegmentedBarItem title="Lägenhet" />
-                    </SegmentedBar>
-                </StackLayout>
-                <StackLayout row="4">
-                    <Label text="När kan vi komma och hämta?" fontWeight="bold" horizontalAlignment="center"/>
-                    <SegmentedBar selectedIndex="0" v-model="selectedTimeFrame">
-                        <SegmentedBarItem title="12h" />
-                        <SegmentedBarItem title="24h" />
-                        <SegmentedBarItem title="48h" />
-                        <SegmentedBarItem title="72h" />
-                    </SegmentedBar>
-                </StackLayout>
-                <StackLayout row="6">
-                    <Label text="Panthämtare" horizontalAlignment="center" fontWeight="bold"/>
-                    <DropDown ref="pantRetrieversDropDown" :items="pantRetrievers" selectedIndex="selectedPantRetriever"/>
-                </StackLayout>
-                <StackLayout row="8" v-if="selectedPropertyType == 1">
-                    <Label textWrap="true" text="Vilken trappuppgång och våning kommer du ställa ut panten?" fontWeight="bold" horizontalAlignment="center"/>
-                    <TextField hint="våning/trappuppgång" v-model="floorInfo"/>
-                </StackLayout>
-                <StackLayout row="10">
-                    <Label text="Övrig information?" fontWeight="bold" horizontalAlignment="center"/>
-                    <TextView v-model="otherInfo"/>
-                </StackLayout>
-                <StackLayout row="12" horizontalAlignment="center">
-                    <Button @onTap="onBookTap" text="Boka hämtning"/>
-                    <Button @onTap="onChangeLocationTap" text="Ändra plats"/>
-                    <Button @onTap="onCancelTap" text="Avbryt"/>
-                </StackLayout>
-            </GridLayout>
-        </ScrollView>-->
+        </ScrollView>
     </Page>
 </template>
 
@@ -79,7 +55,7 @@
     data() {
       return {
         selectedPropertyType: 0,
-        selectedTimeFrame: 0,
+        selectedTimeFrame: 4,
         pantRetrievers: ["Alla tillgängliga", "Gävle Ridklubb 1km"],
         selectedPantRetriever: 0,
         floorInfo: null,
